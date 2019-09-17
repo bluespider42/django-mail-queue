45d9a8d36eb6d6957d93113761f6b5fbbf2d104a
Chris (Someguy123) <info@someguy123.com>
2019-09-17 03:31:37 +0100

    v3.2.2 - Fix tests on modern Django versions

--- mailqueue/__init__.py
+++ mailqueue/__init__.py
@@ -1,3 +1,3 @@
-VERSION = '3.2.1'
+VERSION = '3.2.2'
 
 default_app_config = 'mailqueue.apps.MailQueueConfig'
--- testsettings.py
+++ testsettings.py
@@ -6,6 +6,10 @@ DATABASES = {
 }
 
 INSTALLED_APPS = (
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.messages',
     'mailqueue',
 )
 
