7106f28dd3983a246e50c2ab7b267f0ec462a396
bluespider42 <bluespider42@users.noreply.github.com>
2022-01-21 15:55:51 +0000

    changed send retry

--- mailqueue/tasks.py
+++ mailqueue/tasks.py
@@ -5,11 +5,14 @@ from celery import shared_task
 from .models import MailerMessage
 
 
-@shared_task(name="tasks.send_mail", default_retry_delay=5, max_retries=5)
+@shared_task(name="tasks.send_mail", default_retry_delay=5*60, max_retries=5)
 def send_mail(pk):
-    message = MailerMessage.objects.get(pk=pk)
-    message._send()
+    try:
+        message = MailerMessage.objects.get(pk=pk)
+    except MailerMessage.DoesNotExist:
+        send_mail.retry([pk])
 
+    message._send()
     # Retry when message is not sent
     if not message.sent:
         send_mail.retry([message.pk])
